daebdbaeda75bf1ef0c2fd587193f19d7db01696
adelplace <alexandre.delplacemille@gmail.com>
2023-11-18 22:16:54 +0100

    update

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.local/bin ~/.local/bin
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/i3 ~/.config/i3
+
