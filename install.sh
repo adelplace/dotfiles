40e3167bfeb48ca005ce946b94181e5a2a863883
adelplace <alexandre.delplacemille@gmail.com>
2023-11-19 20:57:48 +0100

    rm .zshrc

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
+mkdir ~/.local
+mkdir ~/.config
+rm ~/.zshrc
+rm ~/.zprofile
+rm -rf ~/.config/i3
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/polybar
 ln -s ~/dotfiles/.local/bin ~/.local/bin
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/polybar ~/.config/polybar
 ln -s ~/dotfiles/.config/i3 ~/.config/i3
 
