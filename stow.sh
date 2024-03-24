c54b090a00c1596d5157539377abd9554f466456
adelplace <alexandre.delplacemille@gmail.com>
2024-03-24 10:31:33 +0100

    add npm

--- stow.sh
+++ stow.sh
@@ -1,8 +1,9 @@
-stow fonts --adopt
-stow foot --adopt
-stow bin --adopt
-stow zsh --adopt
-stow tmux --adopt
-stow vim --adopt
-stow sway --adopt
-stow waybar --adopt
+stow fonts
+stow foot
+stow bin
+stow zsh
+stow tmux
+stow vim
+stow sway
+stow waybar
+stow npm
