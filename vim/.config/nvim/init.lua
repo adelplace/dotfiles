af3785fd86d3e984ca4766c5cbada2e83093890f
adelplace <alexandre.delplacemille@gmail.com>
2024-07-30 11:58:16 +0200

    update

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,4 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-require("lspconfig").tsserver.setup({})
+require("lspconfig").tsserver.setup({
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = "relative",
+      importModuleSpecifierEnding = "minimal",
+    },
+  },
+})
