0bf68bbb3c77596e55a2cdd78502b5ba60383282
adelplace <alexandre.delplacemille@gmail.com>
2025-03-05 17:38:16 +0100

    add chatgpt

---
+++ vim/.config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,44 @@
+require("which-key").add({
+  { "<leader>X", group = "ChatGPT" },
+  {
+    mode = { "n", "v" },
+    { "<leader>Xc", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+    {
+      "<leader>Xe",
+      "<cmd>ChatGPTEditWithInstruction<CR>",
+      desc = "Edit with instruction",
+    },
+    {
+      "<leader>Xg",
+      "<cmd>ChatGPTRun grammar_correction<CR>",
+      desc = "Grammar Correction",
+    },
+    { "<leader>Xt", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+    { "<leader>Xk", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+    { "<leader>Xd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+    { "<leader>Xa", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+    { "<leader>Xc", "<cmd>ChatGPTCompleteCode<CR>", desc = "Complete" },
+    {
+      "<leader>Xo",
+      "<cmd>ChatGPTRun optimize_code<CR>",
+      desc = "Optimize Code",
+    },
+    { "<leader>Xs", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+    { "<leader>Xf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+    {
+      "<leader>Xx",
+      "<cmd>ChatGPTRun explain_code<CR>",
+      desc = "Explain Code",
+    },
+    {
+      "<leader>Xr",
+      "<cmd>ChatGPTRun roxygen_edit<CR>",
+      desc = "Roxygen Edit",
+    },
+    {
+      "<leader>Xl",
+      "<cmd>ChatGPTRun code_readability_analysis<CR>",
+      desc = "Code Readability Analysis",
+    },
+  },
+})
