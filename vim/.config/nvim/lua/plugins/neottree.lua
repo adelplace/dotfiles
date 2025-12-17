87535976e57e9765093a83cc6b1812dda8081311
Alexandre Delplace <alexandre.delplacemille@gmail.com>
2025-12-17 23:10:00 +0100

    add incremental selection

--- vim/.config/nvim/lua/plugins/neottree.lua
+++ vim/.config/nvim/lua/plugins/neottree.lua
@@ -5,19 +5,5 @@ return {
     window = {
       position = "right",
     },
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_by_name = {
-          ".git",
-        },
-      },
-    },
-    default_component_configs = {
-      git_base = "master",
-    },
-  },
-  keys = {
-    { "<leader>ge", "<cmd>:Neotree git_status git_base=master<cr>", desc = "󰙅 Git Explorer" },
   },
 }
---
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+  },
+  {
+    {
+      "MeanderingProgrammer/treesitter-modules.nvim",
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      opts = {
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<A-o>",
+            node_incremental = "<A-o>",
+            scope_incremental = "<A-O>",
+            node_decremental = "<A-i>",
+          },
+        },
+      },
+    },
+  },
+}
